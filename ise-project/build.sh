bbeecd74d230b0820aefeefda64b989438cb6010
Luis Correia <luis.f.correia@gmail.com>
2026-02-08 00:25:05 +0000

    improve build scripts

--- ise-project/build.sh
+++ ise-project/build.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 set -o pipefail
-
 MODULE=timex_mainboard
 
 # cleanup
-
-rm -rf 'xlnx_auto_0_xdb' '_xmsgs' 'xst' '_impact.cmd' '_impact.log' 'netlist.lst' 'timex_interface.bld' 'timex_interface.gyd' 'timex_interface.jed' 'timex_interface.lso' 'timex_interface.mfd' 'timex_interface.ngc' 'timex_interface.ngd' 'timex_interface.ngr' 'timex_interface.pad' 'timex_interface.pnx' 'timex_interface.rpt' 'timex_interface.srf' 'timex_interface.vm6' 'timex_interface.xml' 'timex_interface_build.xml' 'timex_interface_ngdbuild.xrpt' 'timex_interface_pad.csv' 'timex_interface_xst.xrpt' 'tmperr.err'
+. ./clean.sh
 
 xst -ifn $MODULE.xst -ofn $MODULE.srf
 if [ $? -ne 0 ]; then
--- ise-project/clean.sh
+++ ise-project/clean.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # cleanup
-
-rm -rf 'xlnx_auto_0_xdb' '_xmsgs' 'xst' '_impact.cmd' '_impact.log' 'netlist.lst' 'timex_mainboard.bld' 'timex_mainboard.gyd' 'timex_mainboard.jed' 'timex_mainboard.lso' 'timex_mainboard.mfd' 'timex_mainboard.ngc' 'timex_mainboard.ngd' 'timex_mainboard.ngr' 'timex_mainboard.pad' 'timex_mainboard.pnx' 'timex_mainboard.rpt' 'timex_mainboard.srf' 'timex_mainboard.vm6' 'timex_mainboard.xml' 'timex_mainboard_build.xml' 'timex_mainboard_ngdbuild.xrpt' 'timex_mainboard_pad.csv' 'timex_mainboard_xst.xrpt' 'tmperr.err'
-
+set -o pipefail
+MODULE=timex_mainboard
+shopt -s extglob
+rm -rf !(build.sh|clean.sh|$MODULE.prj|$MODULE.ucf|$MODULE.xst|$MODULE.v)
